355f3084dcf88af233d608b6f96214291e328f02
Tobi <Tobi@jacobs.rocks>
2021-11-24 13:25:27 +0100

    Add Link to website

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import imageio
 import PySimpleGUI as sg
 import os
+import webbrowser
 
 
 class GUI ():
     def __init__(self):
         sg.theme('DarkAmber')
         self.layout = [
-            [sg.Text('Wähle das Video aus', size=(20, 1), font=("Helvetica", 25), key="text")],
+            [sg.Text('How many Frames per second to export', size=(35, 1), font=("Helvetica", 15), key="text_export")],
             [sg.Input(default_text="1")],
+            [sg.Text('Choose your video', size=(20, 1), font=("Helvetica", 15), key="text")],
             [sg.In(), sg.FileBrowse(file_types=(("Video Files", "*.mp4"),))],
             [sg.Button('Split', bind_return_key=True, key='split')],
-            [sg.Text('TJ 2021 V1.0 Vitter')]]
+            [sg.Text('TJ 2021 V1.0 Vitter https://2090s-games.de', click_submits=True, key="LINK")]]
 
     def start(self):
         self.window = sg.Window('Vitter - Video Splitter', self.layout)
 
-        # Display and interact with the Window using an Event Loop
         while True:
             event, values = self.window.read()
-            # See if user wants to quit or window was closed
             if event == sg.WINDOW_CLOSED or event == 'Quit':
                 break
             if event == 'split':
-                #print(values[0])
                 path, filename = os.path.split(values[1])
                 try:
                     os.mkdir(path + "/tmp")
@@ -36,6 +35,8 @@ class GUI ():
                 for number, frame in enumerate(reader):
                     if number % int(values[0]) == 0:
                         imageio.imwrite(f'{path}/tmp/{number}_{filename}.jpg', frame)
+            elif event == 'LINK':
+                webbrowser.open_new("https://2090s-games.de")
         self.window.close
 
 
